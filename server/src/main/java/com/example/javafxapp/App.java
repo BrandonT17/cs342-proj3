478e0191dc0f19e1c60cbdf1ade91eb2d3585f6a
Brandon Thach <brandonthach17@gmail.com>
2025-04-10 13:22:53 -0500

    pom.xml fix

--- server/src/main/java/com/example/javafxapp/App.java
+++ server/src/main/java/com/example/javafxapp/App.java
@@ -1,13 +1,21 @@
 package com.example.javafxapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
